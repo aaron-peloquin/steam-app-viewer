f7635488a9255e1c42459de6439cfa11a380196d
Aaron P <aaron4uk@gmail.com>
2019-06-23 00:14:03 -0500

    neat

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const ListItem = ({number = 0}: {number?: number}) => {
   const oldNumber = React.useRef(number)
   React.useEffect(() => {
-    console.log({ number, oldNumber: oldNumber.current, x: Math.random() })
+    // Can still check `previousProps`, but must be done only when needed
+    console.log({ number, oldNumber: oldNumber.current })
     oldNumber.current = number
   })
   return <div>List Item {`#${number}`}</div>
